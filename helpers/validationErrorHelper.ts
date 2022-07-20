65c557654ed0f05069de900a79b8693d0aab762a
Agustin Gutierrez <agustinguti123@gmail.com>
2022-07-20 19:32:46 -0300

    Minor fixes

--- helpers/validationErrorHelper.ts
+++ helpers/validationErrorHelper.ts
@@ -2,22 +2,22 @@ export const validateErrors = (errors: any) => {
   errors.forEach((err: any) => {
     switch (err.code) {
       case "any.required":
-        err.message = `Error: '${err.local.label}' is required`;
+        err.message = `WrongInput: ${err.local.label as string} is required`;
         break;
       case "string.empty":
-        err.message = `Error: '${err.local.label}' should not be empty`;
+        err.message = `WrongInput: ${err.local.label} should not be empty`;
         break;
       case "string.alphanum":
-        err.message = `Error: '${err.local.label}' must have valid format`;
+        err.message = `WrongInput: ${err.local.label} must have valid format`;
         break;
       case "string.email":
-        err.message = `Error: '${err.local.value}' should be valid email`;
+        err.message = `WrongInput: ${err.local.value} should be valid email`;
         break;
       case "string.min":
-        err.message = `Error: '${err.local.label}' should have at least ${err.local.limit} characters`;
+        err.message = `WrongInput: ${err.local.label} should have at least ${err.local.limit} characters`;
         break;
       case "string.max":
-        err.message = `Error: '${err.local.label}' should have at most ${err.local.limit} characters`;
+        err.message = `WrongInput: ${err.local.label} should have at most ${err.local.limit} characters`;
         break;
       default:
         break;
