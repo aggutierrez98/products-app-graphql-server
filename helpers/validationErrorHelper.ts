bba3ec1db6fd6bcf641fe6eb4ae5cd45ccca2f9a
Agustin Gutierrez <agustinguti123@gmail.com>
2022-07-20 19:38:59 -0300

    Minor fixes

--- helpers/validationErrorHelper.ts
+++ helpers/validationErrorHelper.ts
@@ -2,22 +2,22 @@ export const validateErrors = (errors: any) => {
   errors.forEach((err: any) => {
     switch (err.code) {
       case "any.required":
-        err.message = `WrongInput: ${err.local.label as string} is required`;
+        err.message = `Field ${err.local.label as string} is required`;
         break;
       case "string.empty":
-        err.message = `WrongInput: ${err.local.label} should not be empty`;
+        err.message = `Field ${err.local.label} should not be empty`;
         break;
       case "string.alphanum":
-        err.message = `WrongInput: ${err.local.label} must have valid format`;
+        err.message = `Field ${err.local.label} must have valid format`;
         break;
       case "string.email":
-        err.message = `WrongInput: ${err.local.value} should be valid email`;
+        err.message = `Field email shoud be valid email`;
         break;
       case "string.min":
-        err.message = `WrongInput: ${err.local.label} should have at least ${err.local.limit} characters`;
+        err.message = `Field ${err.local.label} should have at least ${err.local.limit} characters`;
         break;
       case "string.max":
-        err.message = `WrongInput: ${err.local.label} should have at most ${err.local.limit} characters`;
+        err.message = `Field ${err.local.label} should have at most ${err.local.limit} characters`;
         break;
       default:
         break;
